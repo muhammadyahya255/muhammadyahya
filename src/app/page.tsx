169442661e12ce9230c251c8e06cc32f52db96e4
Yahya-naveed <muhamamdyahya2@gmail.com>
2024-08-18 01:44:19 -0700

    forward to js project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
-  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/m.y.k.png" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
+      <p className="text-lg font-medium">
+        “My name is muhammad yahya. I from rahim yar khan.I am complete my assigment.”
       </p>
     </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+      MUHAMMADYAHYA
       </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
+      <div className="text-slate-700 dark:text-slate-500">
+        Staff Engineer, pakistan
       </div>
     </figcaption>
   </div>
